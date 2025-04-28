1e20ccf37e0710c9dc066b36e937bfaed6e3bcbe
handy <lihandi@gmail.com>
2025-04-28 16:58:38 +0800

    make Konva work

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: 'canvas' }] // required to make Konva & react-konva work
+    return config
+  },
   experimental: {
     authInterrupts: true,
   },
